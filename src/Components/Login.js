9a73478605e5e5ba281a37e09c1f445556957f95
Harsh Dutta Tewari <103374169+Harsh-Tewari@users.noreply.github.com>
2022-09-07 11:46:02 +0530

    Added Some of the Links , Routes and Buttons

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,130 +1,144 @@
 import React from "react";
 import { useState } from "react";
 import "../CSS/Login.css";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 // import { Link } from 'react-router-dom'
 import { useNavigate } from "react-router-dom";
 import logo from "../images/Healthtrack logo.png";
 
 export default function Login() {
+  const navigate = useNavigate();
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
 
-    const navigate=useNavigate();
-    const [email, setemail] = useState("");
-    const [password, setpassword] = useState("");
+  const handleClick = async (e) => {
+    // This would not refresh the page
+    e.preventDefault();
 
-    const handleClick = async (e) => {
-        // This would not refresh the page
-        e.preventDefault();
-
-        const data = { email, password };
-        if(email.length===0 || password.length===0){
-            toast.error('Email or password cannot be blank', {
-                position: "top-left",
-                autoClose: 3000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                });
-                return  
-        }
-        const res = await fetch('/api/signin', {
-            method: "POST",
-            headers: {
-                //always use this
-                'content-type': "application/json"
-            },
-            body: JSON.stringify(data)
-        })
-    
-        const check = await res.json();
-       
-
-        if (check.success) {
-            localStorage.setItem("email",email)
-            setemail("");
-            setpassword("");
-            toast.success('Successfully logged in', {
-                position: "top-left",
-                autoClose: 2000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                });
-                setTimeout(() => {
-                    navigate("/Dashboard");
-                }, 3000);
-            
-            // alert("Sign In Completed");
-            
-
-        }
-        else if (!check.success ) {
-
-            
-            toast.error(check.error, {
-                position: "top-left",
-                autoClose: 3000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                });
-            setemail("");
-            setpassword("");
-
-        }
-        
+    const data = { email, password };
+    if (email.length === 0 || password.length === 0) {
+      toast.error("Email or password cannot be blank", {
+        position: "top-left",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      return;
     }
+    const res = await fetch("/api/signin", {
+      method: "POST",
+      headers: {
+        //always use this
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
 
+    const check = await res.json();
 
-    const onChange = (req) => {
-       
-        if (req.target.name === "email") { setemail(req.target.value) }
-        else if (req.target.name === "password") { setpassword(req.target.value) }
+    if (check.success) {
+      localStorage.setItem("email", email);
+      setemail("");
+      setpassword("");
+      toast.success("Successfully logged in", {
+        position: "top-left",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      setTimeout(() => {
+        navigate("/Dashboard");
+      }, 3000);
 
+      // alert("Sign In Completed");
+    } else if (!check.success) {
+      toast.error(check.error, {
+        position: "top-left",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      setemail("");
+      setpassword("");
     }
+  };
 
+  const onChange = (req) => {
+    if (req.target.name === "email") {
+      setemail(req.target.value);
+    } else if (req.target.name === "password") {
+      setpassword(req.target.value);
+    }
+  };
 
-    return (
-        <>
-            <ToastContainer
-                position="top-left"
-                autoClose={3000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-            />
-            <div className="text-center" style={{minHeight:"100vh"}} >
+  return (
+    <>
+      <ToastContainer
+        position="top-left"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <div className="text-center" style={{ minHeight: "100vh" }}>
+        <main className="form-signin w-100 m-auto">
+          <form method="POST">
+            <img className="mb-1" src={logo} alt="" width="240" height="140" />
 
-                <main className="form-signin w-100 m-auto">
-                    <form method='POST' >
-                        <img className="mb-1" src={logo} alt="" width="240" height="140" />
-
-                        <div className="form-floating">
-                            <input type="email" className="form-control" value={email} id='email' name='email' placeholder="name@example.com" onChange={onChange} autoComplete='off' required/>
-                            <label htmlFor="floatingInput">Email address</label>
-                        </div>
-                        <div className="form-floating">
-                            <input type="password" className="form-control" value={password} id='password' name='password' placeholder="Password" onChange={onChange}  required/>
-                            <label htmlFor="floatingPassword">Password</label>
-                        </div>
-                        <button className="w-100 btn btn-lg btn-dark" onClick={handleClick}>Sign in</button>
-                        <p className="mt-5 mb-3 text-muted">&copy;2022</p>
-                    </form>
-                </main>
-
+            <div className="form-floating">
+              <input
+                type="email"
+                className="form-control"
+                value={email}
+                id="email"
+                name="email"
+                placeholder="name@example.com"
+                onChange={onChange}
+                autoComplete="off"
+                required
+              />
+              <label htmlFor="floatingInput">Email address</label>
             </div>
-        </>
-    );
+            <div className="form-floating">
+              <input
+                type="password"
+                className="form-control"
+                value={password}
+                id="password"
+                name="password"
+                placeholder="Password"
+                onChange={onChange}
+                required
+              />
+              <label htmlFor="floatingPassword">Password</label>
+            </div>
+            <button className="w-100 btn btn-lg btn-dark" onClick={handleClick}>
+              Sign in
+            </button>
+            <br />
+            <br />
+            <a className="w-100 btn btn-lg btn-dark" href="/Sign">
+              Sign Up
+            </a>
+            <p className="mt-5 mb-3 text-muted">&copy;2022</p>
+          </form>
+        </main>
+      </div>
+    </>
+  );
 }
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -3,156 +3,179 @@ import { useState } from "react";
 import "../CSS/Signup.css";
 import { Link } from "react-router-dom";
 import logo from "../images/Rectangle 1.png";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
 export default function Signup() {
-    const navigate=useNavigate();
+  const navigate = useNavigate();
 
-    const [name, setname] = useState("")
-    const [email, setemail] = useState("")
-    const [password, setpassword] = useState("")
-    const [cpassword, setcpassword] = useState("")
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [cpassword, setcpassword] = useState("");
 
-    const handleClick = async (e) => {
-        e.preventDefault();
+  const handleClick = async (e) => {
+    e.preventDefault();
 
-        
-        if(name==="" || email==="" || password==="" || cpassword===""){
-            toast.error('Credentials not proper', {
-                position: "top-left",
-                autoClose: 2000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                });
-                return  
-        }
-        else if (password !== cpassword) {
+    if (name === "" || email === "" || password === "" || cpassword === "") {
+      toast.error("Credentials not proper", {
+        position: "top-left",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      return;
+    } else if (password !== cpassword) {
+      alert("password mismatch");
+    } else {
+      const data = { name, email, password };
 
-            alert("password mismatch");
+      const res = await fetch("api/register", {
+        method: "POST",
+        headers: {
+          //always use this
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
 
-        }
+      const check = await res.json();
 
-        else {
-            const data = { name, email, password};
+      if (check.success) {
+        toast.success("Thanks ,for registering with us", {
+          position: "top-left",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
 
-            
-            
-            const res = await fetch('api/register', {
-                method: "POST",
-                headers: {
-                    //always use this
-                    'content-type': "application/json"
-                },
-                body: JSON.stringify(data)
-            })
-            
-            
-            const check = await res.json();
-            
+        setname("");
+        setpassword("");
+        setcpassword("");
+        setemail("");
 
-            if (check.success) {
-                toast.success('Thanks ,for registering with us', {
-                    position: "top-left",
-                    autoClose: 2000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    });
-                    
-                setname("");
-                setpassword("");
-                setcpassword("");
-                setemail("");
-
-                setTimeout(() => {
-                    navigate("/Login")
-                }, 3000);
-            }
-            else {
-                toast.error(check.error, {
-                    position: "top-left",
-                    autoClose: 2000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    });
-                setname("");
-                setpassword("");
-                setcpassword("");
-                setemail("");
-            }
-            
-        }
+        setTimeout(() => {
+          navigate("/Login");
+        }, 3000);
+      } else {
+        toast.error(check.error, {
+          position: "top-left",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+        setname("");
+        setpassword("");
+        setcpassword("");
+        setemail("");
+      }
     }
+  };
 
-    const onChange = (req) => {
-        
-        if (req.target.name === "name") { setname(req.target.value) }
-        else if (req.target.name === "email") { setemail(req.target.value) }
-        else if (req.target.name === "password") { setpassword(req.target.value) }
-        else if (req.target.name === "cpassword") { setcpassword(req.target.value) }
+  const onChange = (req) => {
+    if (req.target.name === "name") {
+      setname(req.target.value);
+    } else if (req.target.name === "email") {
+      setemail(req.target.value);
+    } else if (req.target.name === "password") {
+      setpassword(req.target.value);
+    } else if (req.target.name === "cpassword") {
+      setcpassword(req.target.value);
     }
-
-
-
-
-
+  };
 
   return (
     <>
-    <ToastContainer
-                position="top-left"
-                autoClose={3000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-            />
-    
-    <div className="text-center">
+      <ToastContainer
+        position="top-left"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
 
-            <main className="form-signin w-100 m-auto">
+      <div className="text-center">
+        <main className="form-signin w-100 m-auto">
+          <form method="post" onSubmit={handleClick}>
+            <img className="mb-1" src={logo} alt="" width="240" height="140" />
+            {/* <h1 className="h3 mb-3 fw-normal">Please sign in</h1> */}
 
-                <form method='post' onSubmit={handleClick}>
-                    <img className="mb-1" src={logo} alt="" width="240" height="140" />
-                    {/* <h1 className="h3 mb-3 fw-normal">Please sign in</h1> */}
-
-                    <div className="form-floating">
-                        <input type="text" className="form-control" value={name} id='name' name='name' placeholder="Enter your name" onChange={onChange} />
-                        <label htmlFor="floatingName">Name</label>
-                    </div>
-                    <div className="form-floating">
-                        <input type="email" className="form-control" value={email} id='email' name='email' placeholder="name@example.com" onChange={onChange} />
-                        <label htmlFor="floatingInput">Email address</label>
-                    </div>
-                    <div className="form-floating">
-                        <input type="password" className="form-control" value={password} id='password' name='password' placeholder="Password" onChange={onChange} />
-                        <label htmlFor="floatingPassword">Password</label>
-                    </div>
-                    <div className="form-floating">
-                        <input type="password" className="form-control" value={cpassword} id='cpassword' name='cpassword' placeholder="Password" onChange={onChange} />
-                        <label htmlFor="floatingConfirmPassword"> Confirm password</label>
-                    </div>
-                    <button className="w-100 btn btn-lg btn-dark" type="submit">Sign up</button>
-                    <p className="mt-5 mb-3 text-muted">&copy;2022</p>
-                    <span>Already a user? </span>
-                    <Link className="sign-in-link" to={'/Login'}>Sign in</Link>
-                </form>
-            </main>
-
-
-        </div>
-        </>
+            <div className="form-floating">
+              <input
+                type="text"
+                className="form-control"
+                value={name}
+                id="name"
+                name="name"
+                placeholder="Enter your name"
+                onChange={onChange}
+              />
+              <label htmlFor="floatingName">Name</label>
+            </div>
+            <div className="form-floating">
+              <input
+                type="email"
+                className="form-control"
+                value={email}
+                id="email"
+                name="email"
+                placeholder="name@example.com"
+                onChange={onChange}
+              />
+              <label htmlFor="floatingInput">Email address</label>
+            </div>
+            <div className="form-floating">
+              <input
+                type="password"
+                className="form-control"
+                value={password}
+                id="password"
+                name="password"
+                placeholder="Password"
+                onChange={onChange}
+              />
+              <label htmlFor="floatingPassword">Password</label>
+            </div>
+            <div className="form-floating">
+              <input
+                type="password"
+                className="form-control"
+                value={cpassword}
+                id="cpassword"
+                name="cpassword"
+                placeholder="Password"
+                onChange={onChange}
+              />
+              <label htmlFor="floatingConfirmPassword"> Confirm password</label>
+            </div>
+            <button className="w-100 btn btn-lg btn-dark" type="submit">
+              Sign up
+            </button>
+            <span>Back to </span>
+            <Link className="sign-in-link" to={"/"}>
+              Home Page
+            </Link>
+            <p className="mt-5 mb-3 text-muted">&copy;2022</p>
+            <span>Already a user? </span>
+            <Link className="sign-in-link" to={"/Login"}>
+              Sign in
+            </Link>
+          </form>
+        </main>
+      </div>
+    </>
   );
 }
