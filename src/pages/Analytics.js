5018711da4e251afdbc9966496f7944e27f2c4cd
Harsh Dutta Tewari <103374169+Harsh-Tewari@users.noreply.github.com>
2022-09-07 17:48:38 +0530

    changed legends

--- src/pages/Analytics.js
+++ src/pages/Analytics.js
@@ -1,143 +1,109 @@
 import React from "react";
 import Sidebar from "../Components/Sidebar";
 import { useEffect } from "react";
-import {useNavigate} from 'react-router-dom';
-import Chart from 'chart.js'
+import { useNavigate } from "react-router-dom";
+import Chart from "chart.js";
 const Analytics = () => {
-
-  const navigate=useNavigate();
+  const navigate = useNavigate();
   useEffect(() => {
-    if(!localStorage.getItem("email")){
-      
-      navigate("/Login")
-     return
+    if (!localStorage.getItem("email")) {
+      navigate("/Login");
+      return;
     }
-        const ysugar=[];
-        const xlabels = [];
-        const yblood = [];
-        chartIt()
-        async function chartIt() {
-            
-            await getData();
-            
-            const ctx = document.getElementById('chart').getContext('2d');
+    const ysugar = [];
+    const xlabels = [];
+    const yblood = [];
+    chartIt();
+    async function chartIt() {
+      await getData();
 
-            new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: xlabels,
-                    datasets: [{
-                        label: 'Blood report',
-                        data: yblood,
-                        borderColor: [
-                            'blue',
-                        ],
-                        borderWidth: 1,
+      const ctx = document.getElementById("chart").getContext("2d");
 
-                    },
-                    {
-                        label: 'Glucose report',
-                        data: ysugar,
-                        borderColor: [
-                            'red',
-                        ],
-                        borderWidth: 1,
-                    }
-                ]
+      new Chart(ctx, {
+        type: "line",
+        data: {
+          labels: xlabels,
+          datasets: [
+            {
+              label: "Systolic Blood Pressure(mmHg)",
+              data: yblood,
+              borderColor: ["blue"],
+              borderWidth: 1,
+            },
+            {
+              label: "Systolic Blood Pressure(mmHg)",
+              data: ysugar,
+              borderColor: ["red"],
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              ticks: {
+                type: "linear",
+
+                callback: function (value, index, ticks) {
+                  return value + "mmHg";
                 },
-                options: {
-                    scales: {
-                        y: {
-                            ticks: {
-                                type: 'linear',
+              },
+            },
+          },
+        },
+      });
+    }
+    async function getData() {
+      const email = localStorage.getItem("email");
+      const newD = { email };
+      const response = await fetch("/api/fetchGraph", {
+        method: "POST",
 
-                                callback: function (value, index, ticks) {
-                                    return value + "mmHg"
-                                }
-                            }
-                        }
-                    }
-                }
-            });
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newD),
+      });
 
-        }
-        async function getData() {
-          const email=localStorage.getItem("email")
-          const newD={email}
-            const response = await fetch('/api/fetchGraph', {
+      const b = await response.json();
 
-                method: 'POST',
+      b.data.sort((a, b) => {
+        return new Date(a.date) - new Date(b.date);
+      });
 
+      for (let i = 0; i < b.data.length; i++) {
+        const sugar = b.data[i].sugar;
+        const blood = b.data[i].blood;
+        const date = b.data[i].date;
+        ysugar.push(sugar);
+        xlabels.push(date);
+        yblood.push(blood);
+      }
+    }
 
-                headers: {
-                    'Content-Type': 'application/json',
-                    
-
-                },
-                body:JSON.stringify(newD)
-
-            }
-            );
-            
-            const b = await response.json();
-            
-
-            b.data.sort((a, b) => {
-              return new Date(a.date) - new Date(b.date);
-            });
-            
-            
-            
-           
-
-            for (let i = 0; i < b.data.length; i++) {
-                
-                const sugar=b.data[i].sugar;
-                const blood = b.data[i].blood;
-                const date = b.data[i].date;
-                ysugar.push(sugar);
-                xlabels.push(date);
-                yblood.push(blood);
-
-            }
-            
-
-
-        }
-
-
-
- 
-  // eslint-disable-next-line
-  }, [])
+    // eslint-disable-next-line
+  }, []);
   return (
     <div className="pagebg">
       <Sidebar>
         <h1 className="pagebg">Analytics page</h1>
         <div>
-
-            <div className="container-fluid">
-                <div className="row">
-
-                  <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-
-                        
-                        <canvas className="my-4 w-100" id="chart" width="900" height="380">
-
-                        </canvas>
-
-
-                    </main>
-                </div>
-            </div></div>
+          <div className="container-fluid">
+            <div className="row">
+              <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <canvas
+                  className="my-4 w-100"
+                  id="chart"
+                  width="900"
+                  height="380"
+                ></canvas>
+              </main>
+            </div>
+          </div>
+        </div>
       </Sidebar>
     </div>
   );
 };
 
 export default Analytics;
-
-
-
-
-
