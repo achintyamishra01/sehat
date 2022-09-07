dbbeb4deeb7f38fb5403ea1cd50b6b46bc936b00
Harsh Dutta Tewari <103374169+Harsh-Tewari@users.noreply.github.com>
2022-09-07 17:45:24 +0530

    Changed Name

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,110 +1,128 @@
 import React from "react";
 import Sidebar from "../Components/Sidebar";
 import { useState } from "react";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { useEffect } from "react";
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 const About = () => {
-  const navigate=useNavigate();
+  const navigate = useNavigate();
   useEffect(() => {
-    if(!localStorage.getItem("email")){
-      
-      navigate("/Login")
-     return
+    if (!localStorage.getItem("email")) {
+      navigate("/Login");
+      return;
     }
- 
-  // eslint-disable-next-line
-  }, [])
-  const [blood, setblood] = useState("")
-  const [sugar, setsugar] = useState("")
-  const handleChange=(e)=>{
-    if(e.target.name==="blood"){setblood(e.target.value)}
-    if(e.target.name==="sugar"){setsugar(e.target.value)}
-  }
-  const handleSubmit=async(e)=>{
+
+    // eslint-disable-next-line
+  }, []);
+  const [blood, setblood] = useState("");
+  const [sugar, setsugar] = useState("");
+  const handleChange = (e) => {
+    if (e.target.name === "blood") {
+      setblood(e.target.value);
+    }
+    if (e.target.name === "sugar") {
+      setsugar(e.target.value);
+    }
+  };
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    
-         
-     
-   
+
     const d = new Date();
     const date = d.toDateString();
-    const email=localStorage.getItem("email")
-    const data={blood,sugar,date,email}
-    const res = await fetch('/api/addGraph', {
+    const email = localStorage.getItem("email");
+    const data = { blood, sugar, date, email };
+    const res = await fetch("/api/addGraph", {
       method: "POST",
       headers: {
-          //always use this
-          'content-type': "application/json"
+        //always use this
+        "content-type": "application/json",
       },
-      body: JSON.stringify(data)
-  })
-  
-  const check = await res.json();
-if(!check.success){
-
-  toast.error(check.error, {
-    position: "top-left",
-    autoClose: 2000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
+      body: JSON.stringify(data),
     });
 
-}
-else{
-  toast.success("Records saved successfully", {
-    position: "top-left",
-    autoClose: 2000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    });
-    setblood("")
-    setsugar("")
-}
-  
-
-  }
+    const check = await res.json();
+    if (!check.success) {
+      toast.error(check.error, {
+        position: "top-left",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    } else {
+      toast.success("Records saved successfully", {
+        position: "top-left",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      setblood("");
+      setsugar("");
+    }
+  };
   return (
     <>
-     <ToastContainer
-                position="top-left"
-                autoClose={3000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-            />
-    <div className="pagebg" style={{minHeight:"100vh"}}>
-      <Sidebar>
-        <h1 className="pagebg">About page</h1>
-        <form>
-  <div className="mb-3">
-    <label htmlFor="exampleInputEmail1" className="form-label">Blood Count</label>
-    <input type="number" value={blood} onChange={handleChange} className="form-control" name="blood" id="blood" aria-describedby="emailHelp"/>
-   
-  </div>
-  <div className="mb-3">
-    <label htmlFor="exampleInputEmail1" className="form-label">sugar Count</label>
-    <input type="number" className="form-control" onChange={handleChange} value={sugar} name="sugar" id="sugar"/>
-  </div>
- 
-  <button type="submit" className="btn btn-primary" onClick={handleSubmit}>Submit</button>
-</form>
-      </Sidebar>
-    </div>
+      <ToastContainer
+        position="top-left"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <div className="pagebg" style={{ minHeight: "100vh" }}>
+        <Sidebar>
+          <h1 className="pagebg">About page</h1>
+          <form>
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">
+                Systolic Blood Pressure(mmHg)
+              </label>
+              <input
+                type="number"
+                value={blood}
+                onChange={handleChange}
+                className="form-control"
+                name="blood"
+                id="blood"
+                aria-describedby="emailHelp"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">
+                Diastolic Blood Pressure(mmHg)
+              </label>
+              <input
+                type="number"
+                className="form-control"
+                onChange={handleChange}
+                value={sugar}
+                name="sugar"
+                id="sugar"
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="btn btn-primary"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </form>
+        </Sidebar>
+      </div>
     </>
   );
-  
 };
 
 export default About;
